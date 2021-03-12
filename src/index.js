7e8d23a8ce4b0730a560e45dae96b7670e36e1b6
Ilya Denezhkin <Denezhkinia@ya.ru>
2021-03-12 17:15:20 +0300

    feat: problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	return n > 0 ? positive(n) : negative(n)
+}
+function positive(n) {
+	return n.toString().split('').reverse().join('');
+}
+
+function negative(n) {
+	let arr = n.toString().split('').reverse();
+	arr.pop()
+	let res = arr.reduce((arr, i) => arr.concat(i))
+	return res
 }
